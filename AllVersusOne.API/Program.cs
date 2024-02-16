6ce2acde978574f2f6586a6599406ee07a86a760
Preben Nielsen <pbni@netcompany.com>
2024-02-17 00:34:43 +0100

    Perform migrations at startup

--- AllVersusOne.API/Program.cs
+++ AllVersusOne.API/Program.cs
@@ -1,5 +1,7 @@
 using AllVersusOne.API.Conventions;
 using System.Text.Json.Serialization;
+using AllVersusOne.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,7 +22,16 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 });
 
 var app = builder.Build();
-
+using (var scope = builder.Services.BuildServiceProvider().CreateScope())
+{
+    using (var dbContext = scope.ServiceProvider.GetRequiredService<DatabaseContext>())
+    {
+        if (dbContext.Database.GetPendingMigrations().Any())
+        {
+            dbContext.Database.Migrate();
+        }
+    }
+}
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
