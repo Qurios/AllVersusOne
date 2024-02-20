f3988adad8f860a96a45eab30758340681d969a8
Preben Nielsen <pbni@netcompany.com>
2024-02-20 21:24:42 +0100

    Change route to not prefix api

--- AllVersusOne.API/Controllers/UserController.cs
+++ AllVersusOne.API/Controllers/UserController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AllVersusOne.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class UserController(IUserService userService) : ControllerBase
     {
