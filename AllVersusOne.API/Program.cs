f607ae878c3993a312d76aa4f387e018735ee758
Preben Nielsen <pbni@netcompany.com>
2024-02-20 21:12:26 +0100

    Add AzureAppServices logging

--- AllVersusOne.API/Program.cs
+++ AllVersusOne.API/Program.cs
@@ -1,11 +1,10 @@
 using AllVersusOne.API.Conventions;
+using Microsoft.Extensions.Configuration;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -20,6 +19,8 @@ builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
     options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
 });
+
+builder.Services.AddLogging(x => x.AddAzureWebAppDiagnostics());
 
 var app = builder.Build();
 
